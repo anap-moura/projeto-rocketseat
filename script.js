48f1eef35d02361fde598c45e585c850dc322cd9
Ana Paula Moura <ana.mcruz@hotmail.com>
2023-07-11 18:09:13 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle('light')
+
+  //pegar a tag image
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+  if (html.classList.contains('light')){
+    //se tiver light mode, adicionar a imagem light
+    img.setAttribute('src','./assets/avatarr-light.png')
+  } else {
+    //se tiver sem light mode, manter a imagem original
+    img.setAttribute('src','./assets/avatar-dark.png')
+  }
+
+}
